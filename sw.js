9f13a84b98d53fe538234276bad559a3d9f253f7
Yuilovemiyu <157918693+Yuilovemiyu@users.noreply.github.com>
2026-04-28 00:16:14 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,19 @@
-const CACHE_NAME = "hcpg-report-v6";
+const CACHE_NAME = "hcpg-final-v1";
 
-const urlsToCache = [
-  "./",
-  "./index.html"
-];
-
-// INSTALL
-self.addEventListener("install", event => {
+self.addEventListener("install", e => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(["./"])));
 });
 
-// ACTIVATE (remove old caches)
-self.addEventListener("activate", event => {
-  event.waitUntil(
+self.addEventListener("activate", e => {
+  e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.map(k => k !== CACHE_NAME && caches.delete(k))
-      )
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
-// FETCH (network first)
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, clone);
-        });
-        return res;
-      })
-      .catch(() => caches.match(event.request))
-  );
+self.addEventListener("fetch", e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
