124c945bd4ff2a049291eae43a1cebc16f402cde
Yuilovemiyu <157918693+Yuilovemiyu@users.noreply.github.com>
2026-04-27 21:06:26 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,40 @@
-self.addEventListener("install", e => {
-  self.skipWaiting();
+const CACHE_NAME = "hc-pg-sa-cache-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 });
 
-self.addEventListener("fetch", e => {});
+// Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
